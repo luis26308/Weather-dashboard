1826183953cea8f0ad83f476dfc58d023c5b90e1
Luis <lgd26308@gmail.com>
2020-09-19 19:02:43 -0700

    updates

--- script.js
+++ script.js
@@ -2,17 +2,29 @@ $("button").on("click", function (event) {
     event.preventDefault();
     let cityWeather = $("input").val().trim()
     let api_key = "fe4a853726c79035bac73e26c523869e";
-    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityWeather + "&appid=" + api_key
+    let queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityWeather + "&appid=" + api_key
+    let queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityWeather + "&appid=" + api_key
 
 
     $.ajax({
-        url: queryURL,
+        url: queryURL1,
         method: "GET"
     })
 
         .then(function (response) {
-            console.log(queryURL);
             console.log(response);
-
+            $("#cityName").text(response.name)
+            $("#temp").text("Temperature: " + response.main.temp + " degrees")
+            $("#humidity").text("Humidity: " + response.main.humidity + "%")
+            $("#wind").text("Wind speed: " + response.wind.speed + " MPH")
         });
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    })
+
+        .then(function(response) {
+            console.log(response);
+        });
+
 });
