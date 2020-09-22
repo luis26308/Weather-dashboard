bdd0a259f714e429339dd59d8867b25c557c1bb2
Luis <lgd26308@gmail.com>
2020-09-21 21:45:45 -0700

    update again

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
 let cities = [];
-storeHistory();
+
 getHistory();
 
 function storeHistory() {
     localStorage.setItem("cities", JSON.stringify(cities))
+    console.log(cities)
 }
 
 function getHistory() {
     let savedCities = JSON.parse(localStorage.getItem("cities"));
-    if (savedCities !== null){
+    if (savedCities !== null) {
         cities = savedCities
     }
     historyButton();
@@ -17,8 +18,8 @@ function getHistory() {
 
 function historyButton() {
     for (let i = 0; i < cities.length; i++) {
-        let historyB = $("<button>")
-        historyB.text(city[i])
+        let historyB = $("<button>").attr("class", "list-group-item")
+        historyB.text(cities[i])
         $(".history").append(historyB)
     }
 }
@@ -29,10 +30,14 @@ $("button").on("click", function (event) {
     event.preventDefault();
     let cityWeather = $("input").val().trim()
     cities.push(cityWeather)
+    if(cities.length > 8) {
+        cities.shift();
+    }
     let api_key = "fe4a853726c79035bac73e26c523869e";
     let queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityWeather + "&appid=" + api_key
     let queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityWeather + "&appid=" + api_key
-
+    storeHistory();
+    historyButton();
 
     $.ajax({
         url: queryURL1,
@@ -58,24 +63,25 @@ $("button").on("click", function (event) {
             console.log(response);
             let newrow = $("<div>").attr("class", "forecast");
             $("#futureForecast").append(newrow);
-            
+
             for (let i = 0; i < response.list.length; i++) {
                 if (response.list[i].dt_txt.indexOf("15:00:00") !== -1) {
                     let newCol = $("<div>").attr("class", "one-fifth");
-                newrow.append(newCol);
+                    newrow.append(newCol);
 
-                let newCard = $("<div>").attr("class", "card text-white bg-primary");
-                newCol.append(newCard);
+                    let newCard = $("<div>").attr("class", "card text-white bg-primary");
+                    newCol.append(newCard);
 
-                let cardImg = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png");
-                newCard.append(cardImg);
+                    let cardImg = $("<img>").attr("class", "card-img-top").attr("src", "https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + "@2x.png");
+                    newCard.append(cardImg);
 
-                let bodyDiv = $("<div>").attr("class", "card-body");
-                newCard.append(bodyDiv);
+                    let bodyDiv = $("<div>").attr("class", "card-body");
+                    newCard.append(bodyDiv);
 
-                let tempF = Math.floor(( response.list[i].main.temp - 273.15) * 1.80 + 32)
-                bodyDiv.append($("<p>").attr("class", "card-text").html("Temp: " + tempF + " °F"));
-                bodyDiv.append($("<p>").attr("class", "card-text").text("Humidity: " + response.list[i].main.humidity + "%"));
+                    let tempF = Math.floor((response.list[i].main.temp - 273.15) * 1.80 + 32)
+                    bodyDiv.append($("<p>").attr("class", "card-text").text("Temp: " + tempF + " °F"));
+                    bodyDiv.append($("<p>").attr("class", "card-text").text("Humidity: " + response.list[i].main.humidity + "%"));
+                    bodyDiv.append($("<p>").attr("class", "card-text").text("wind: " + response.list[i].wind.speed + " MPH"));
                 }
             }
         });
